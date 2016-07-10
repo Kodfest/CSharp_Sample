9c2f60268e5a5e8ef04eefedfb4476960e123ac1
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-11 01:16:14 +0300

    code added

--- ConditionalOperator2/ConditionalOperator2/Program.cs
+++ ConditionalOperator2/ConditionalOperator2/Program.cs
@@ -10,7 +10,24 @@ namespace ConditionalOperator2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bir Sayı Girin");
 
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine("Sıfır Girildi.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Negatif Sayı Girildi.");
+            }
+            else
+            {
+                Console.WriteLine("Pozitif Sayı Girildi.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
