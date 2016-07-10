17e74b9619c0942cdbd53cbdf96da78ef428ab26
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-10 22:53:28 +0300

    first Commit

---
+++ ConditionalOparetor1/ConditionalOparetor1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOparetor1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine(); // word'e okunan değer atanır
+
+            /*
+             ? öncesinde koşul belirtilir.
+             Bu kodda okuduğumuz değerin uzunluğu (karakter sayısı) 5'e eşit ve küçükse 
+             koşul True değer alır ve ? : arasındaki ilk kısım çalışır.
+             Eğer 5'den daha büyükse koşul False değer alır 
+             ve : sonrasındaki kısım çalışır.
+             
+            Kodda eklediğimiz word den önceki True ve False değerlerinin 
+            özel bir anlamaı yoktur. İfadeleri vurgulamak için eklenmiştir.
+            */
+            word = word.Length <= 5 ? "True " + word : word = "False " + "Uzunluk 5'den büyük";
+
+            Console.WriteLine(word); // word'ün son hali ekrana yazılır.
+
+            Console.ReadKey(); //Ekran bir tuşa basılana kadar bekletilir.
+        }
+    }
+}
