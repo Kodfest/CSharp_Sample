e00367ccc8e596718da62bc575d19c72d51616b6
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-11 18:03:15 +0300

    Code Added

--- Loop_Foreach/Loop_Foreach/Program.cs
+++ Loop_Foreach/Loop_Foreach/Program.cs
@@ -10,6 +10,36 @@ namespace Loop_Foreach
     {
         static void Main(string[] args)
         {
+            /*
+            string tipinde bir şehirler listesi oluşturuldu.
+            3 şehir eklendi.
+            */
+            List<string> Cities = new List<string>();
+            Cities.Add("İzmir");
+            Cities.Add("İstanbul");
+            Cities.Add("Ankara");
+
+            //şeihrler yazdırılıyor
+            foreach (var item in Cities)
+            {
+                Console.WriteLine(item);
+            }
+
+            try
+            {
+                foreach (var item in Cities.ToList())
+                {
+                    // item listeden çıkarılıyor
+                    Cities.Remove(item);
+                    Console.WriteLine("{0} çıkarıldı.", item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
