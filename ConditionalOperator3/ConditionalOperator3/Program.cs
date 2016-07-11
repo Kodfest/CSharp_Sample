8d0aa9726d00525cc0e13a2998530f1f75c48d0f
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-11 15:23:53 +0300

    code added

--- ConditionalOperator3/ConditionalOperator3/Program.cs
+++ ConditionalOperator3/ConditionalOperator3/Program.cs
@@ -10,6 +10,39 @@ namespace ConditionalOperator3
     {
         static void Main(string[] args)
         {
+            /*
+            string ifadelerin içine yazılan 
+            "\n" bir satır alta geçilmesini sağlar
+            */
+            Console.WriteLine("İşlem Seçiniz\n");
+            
+            // İşlem Menüsü
+            Console.WriteLine("1. Satış Bürosu\n2. Muhasebe Bürosu\n3. İnsan Kaynakları\n4. Müşteri Temsilcisi");
+            byte secim = Convert.ToByte(Console.ReadLine());
+            string cikti;
+
+            switch (secim)
+            {
+                case 1:
+                    cikti = "Satış'a Yönlendiriliyorsunuz.";
+                    break;
+                case 2:
+                    cikti = "Muhasebe'ye Yönlendiriliyorsunuz.";
+                    break;
+                case 3:
+                    cikti = "İnsan Kaynaklarına Yönlendiriliyorsunuz.";
+                    break;
+                case 4:
+                    cikti = "Müşteri Temsilcisine Yönlendiriliyorsunuz.";
+                    break;
+
+                default:
+                    cikti = "Yanlış Giriş \nMüşteri Temsilcisine Yönlendiriliyorsunuz.";
+                    break;
+            }
+            Console.WriteLine(cikti);
+
+            Console.ReadKey();
         }
     }
 }
