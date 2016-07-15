4e53e8f816fe60bb3e6c3eef572fd98f8740d06d
Yavuz Gedik <yavuzgedik@outlook.com>
2016-07-15 18:15:32 +0300

    Code Added

--- Loop_DoWhile/Loop_DoWhile/Program.cs
+++ Loop_DoWhile/Loop_DoWhile/Program.cs
@@ -10,12 +10,45 @@ namespace Loop_DoWhile
     {
         static void Main(string[] args)
         {
-            bool state = true;
+            // https://github.com/Kodfest/CSharp_Sample/tree/master/ConditionalOperator3
+            // switch case yapısı örneğîne whîle döngüsü yardmıyla devamlılık eklendî
+            
+            string c;
 
             do
             {
-                //kod
-            } while (state);
+                Console.WriteLine("İşlem Seçiniz\n");
+
+                Console.WriteLine("1. Satış Bürosu\n2. Muhasebe Bürosu\n3. İnsan Kaynakları\n4. Müşteri Temsilcisi");
+                byte secim = Convert.ToByte(Console.ReadLine());
+                string cikti;
+
+                switch (secim)
+                {
+                    case 1:
+                        cikti = "Satış'a Yönlendiriliyorsunuz.";
+                        break;
+                    case 2:
+                        cikti = "Muhasebe'ye Yönlendiriliyorsunuz.";
+                        break;
+                    case 3:
+                        cikti = "İnsan Kaynaklarına Yönlendiriliyorsunuz.";
+                        break;
+                    case 4:
+                        cikti = "Müşteri Temsilcisine Yönlendiriliyorsunuz.";
+                        break;
+
+                    default:
+                        cikti = "Yanlış Giriş \nMüşteri Temsilcisine Yönlendiriliyorsunuz.";
+                        break;
+                }
+                Console.WriteLine(cikti);
+
+                Console.WriteLine("Yenı ıslem yapmak ıstıyor musunuz? (evet|hayır)");
+                c = Convert.ToString(Console.ReadLine());
+                
+
+            } while (c == "evet");
         }
     }
 }
