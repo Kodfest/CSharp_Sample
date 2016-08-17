ab503435b67121ee2e9725f9073cf93c085a235b
meltay <meltay.isilti@ogr.deu.edu.tr>
2016-08-17 21:36:24 +0300

    Linked_List

---
+++ Linked_List/Linked_List/Phone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    public class Phone
+    {
+        public string phoneName { get; set; }
+        public string phoneNumber { get; set; }
+        public decimal phonePrice { get; set; }
+        public Phone(string phoneName, string phoneNumber, decimal phonePrice)
+        {
+            this.phoneName = phoneName;
+            this.phoneNumber = phoneNumber;
+            this.phonePrice = phonePrice;
+        }
+    }
+}
